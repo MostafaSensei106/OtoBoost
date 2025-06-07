89771b605675ff2ab26f861969f624b7bac8cae8
MostafaSensei <wwwmostafagmer100@gmail.com>
2025-06-08 02:33:44 +0300

    Add project files.

---
+++ OtoBoost/Form1.cs
@@ -0,0 +1,15 @@
+namespace OtoBoost
+{
+    public partial class MainScreen : Form
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
